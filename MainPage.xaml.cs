1096708f600630ff524927eb278d0b3f3177a26b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2023-11-08 15:07:17 -0600

    Ugh, seems to be working fine!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,10 +3,19 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	CarouselView carouselView;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		RunAsync();
+	}
+
+	async void RunAsync()
+	{
+		await Task.Delay(5000);
+		carouselView = new CarouselView();
+		MainStackLayout.Children.Add(carouselView);
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
